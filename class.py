8a91683ab5afed7a8a766ff30e02762026841b17
MaissaeTorche <maissae.torche96@gmail.com>
2022-08-24 11:15:57 +0200

    Ajout d'un attribut de classe pour tester

--- class.py
+++ class.py
@@ -2,5 +2,10 @@ class lol():
 
     def __init__(self):
         self.lel="lel"
+<<<<<<< HEAD
         self.isTrue=True 
         self.oui="oiu"
+=======
+        self.isTrue=True
+        self.test="test"
+>>>>>>> b73dd27 (essai ajout)
